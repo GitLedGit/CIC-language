6a16b08de68f13387539068cb761009a194004ae
GitLedGit <118552036+GitLedGit@users.noreply.github.com>
2022-11-28 19:00:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+print("CIC language: Write code: ")
+while True:
+  c = input()
+  if c == "10101":
+    print("on off on off on")
+  if c == "01010":
+    print("off on off on off")
+  if c == "10101#01010":
+    print("on off on off on # off on off on off")
+  if c == "01010#10101":
+    print("off on off on off # on off on off on")
+
+  if c == "11011":
+    print("on on off on on")
+  if c == "00100":
+    print("off off on off off")
